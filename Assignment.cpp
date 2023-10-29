0f4924688737aa7466fc0d471b47f72bacc4ae97
vinay2284 <94819302+vinay2284@users.noreply.github.com>
2023-10-29 23:34:32 +0530

    Update Assignment.cpp
    Added a new functionality for finding diameter of the tree.

--- Assignment.cpp
+++ Assignment.cpp
@@ -29,6 +29,27 @@ int findMax(Node* root)
     return res;
 }
 
+//function to find diameter of a tree
+pair<int,int> diameter(Node* root){
+    if(root==NULL)
+    {
+        pair<int,int> p=make_pair(0,0);
+        return p;
+    }
+    pair<int,int> left=diameter(root->left);
+    pair<int,int> right=diameter(root->right);
+    int op1=left.first;
+    int op2=right.first;
+    int op3=left.second+right.second+1;
+    pair<int,int> ans;
+    ans.first=max(op1,max(op2,op3));
+    ans.second=max(left.second,right.second)+1;
+    return ans;
+}
+int diameterOfBinaryTree(Node* root) {
+    return diameter(root).first-1;
+}
+
 int main(){
     Node *root=new Node(3);
     root->left=new Node(9);
@@ -44,4 +65,6 @@ int main(){
     
     int result=findMax(root);
     cout<<"Max Value in Tree:"<< result;
+
+    cout<<"\nDiameter of the tree: "<<diameterOfBinaryTree(root)<<endl;
 }
